a78fd7aae44cc6b2ef7d420e89ad9e7b10123dc0
magnata19 <dehpacifico@gmail.com>
2024-05-04 12:19:12 -0300

    alterando valor da variavel

--- php/variaveis.php
+++ php/variaveis.php
@@ -5,5 +5,11 @@ $valorDaGorjeta = 30;
 
 $valorDoDiaTotal = $valorDaDiaria + $valorDaGorjeta;
 
+echo "Valor recebido no dia: $valorDoDiaTotal\n";
+
+$valorDaGorjeta = 15;
+
+$valorDoDiaTotal = $valorDaDiaria + $valorDaGorjeta;
+
 echo "Valor recebido no dia: $valorDoDiaTotal";
 ?>
